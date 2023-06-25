f5531fe4a1e75bdb9e45ab9021d1c6b265f5b200
Loic Fuentes <fuentes.loic@gmail.com>
2023-06-25 11:51:34 -0400

    feat: update playground and test to reflect do changes

--- playground/server/api/stripe.ts
+++ playground/server/api/stripe.ts
@@ -1,11 +1,11 @@
+import { defineEventHandler } from 'h3';
 import { useServerStripe } from '#stripe/server'
 
 export default defineEventHandler(async (event) => { 
     const stripe = await useServerStripe(event)
 
-    console.log(stripe)
-
+    console.info("Stripe instance:", stripe)
     return {
-        hello: "stripe",
+        message: 'Inspect your terminal to see stripe server object'
     }
 });
--- test/fixtures/basic/server/api/stripe.ts
+++ test/fixtures/basic/server/api/stripe.ts
@@ -1,8 +1,10 @@
 import { useServerStripe } from '#stripe/server'
+import { defineEventHandler } from 'h3';
 
 export default defineEventHandler(async (event) => { 
     const stripe = await useServerStripe(event)
+
     return {
-        hello: "stripe"
+        api: stripe.PACKAGE_VERSION
     }
 });
