c357af68ce79134152711254e23dd3f5507f7b6b
Loic Fuentes <fuentes.loic@gmail.com>
2023-06-25 11:54:24 -0400

    chore: rephrase doc for early return in server

--- src/runtime/server/services/useServerStripe.ts
+++ src/runtime/server/services/useServerStripe.ts
@@ -13,11 +13,11 @@ import { H3Event } from 'h3';
 export const useServerStripe = async(event: H3Event): Promise<Stripe> => {
   const {stripe: { apiKey, apiVersion }} = useRuntimeConfig()
 
-  // If the Stripe instance is already initialized for the event context, return it
-  if ( event.context._stripe ) return event.context._stripe as Stripe
+  // Return Stripe's instance if already initialized in event context
+  if ( event.context._stripe ) return event.context._stripe
 
   // Stripe's TypeScript definition may throw an error if the apiVersion is not set
-  // We can safely ignore this error by using the @ts-ignore directive
+  // We are safely ignoring this error by using @ts-ignore directive
   // @docs — https://stripe.com/docs/api/versioning
   // @ts-ignore
   const stripe = new Stripe(apiKey, { apiVersion })
