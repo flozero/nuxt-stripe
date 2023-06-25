e37e46409d778e8a74059989a41a761e378d2086
Loic Fuentes <fuentes.loic@gmail.com>
2023-06-25 12:10:03 -0400

    chore: update test

--- test/fixtures/basic/server/api/stripe.ts
+++ test/fixtures/basic/server/api/stripe.ts
@@ -5,6 +5,6 @@ export default defineEventHandler(async (event) => {
     const stripe = await useServerStripe(event)
 
     return {
-        api: stripe.PACKAGE_VERSION
+        version: stripe.VERSION
     }
 });
