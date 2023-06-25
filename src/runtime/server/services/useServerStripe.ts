51d2389e4bd80cad29e1a8de7995f87d73cb8439
Loic Fuentes <fuentes.loic@gmail.com>
2023-06-25 11:51:01 -0400

    chore: add doc in plugin

--- src/runtime/server/services/useServerStripe.ts
+++ src/runtime/server/services/useServerStripe.ts
@@ -1,14 +1,28 @@
 import Stripe from "stripe"
 import { useRuntimeConfig } from '#imports'
+import { H3Event } from 'h3';
 
-export const useServerStripe = async(event: any) => {
-  const { stripe: { apiKey }, public: { stripe: { apiVersion }} } = useRuntimeConfig()
+/**
+ * useServerStripe is a utility function that initializes and returns a Stripe instance
+ * for server-side usage in Nuxt 3. It ensures that only one instance of Stripe is created
+ * per event context, avoiding unnecessary re-initializations.
+ *
+ * @param {H3Event} event - The event object passed to the Nuxt 3 event handler
+ * @return {Promise<Stripe>} - A Promise that resolves to the Stripe server instance for the event context
+ */
+export const useServerStripe = async(event: H3Event): Promise<Stripe> => {
+  const {stripe: { apiKey, apiVersion }} = useRuntimeConfig()
 
-  if ( event.context._stripe ) return event.context._stripe
+  // If the Stripe instance is already initialized for the event context, return it
+  if ( event.context._stripe ) return event.context._stripe as Stripe
 
-  // As Stripe make ts fail when not setting the apiVersion, we need to ignore the error
+  // Stripe's TypeScript definition may throw an error if the apiVersion is not set
+  // We can safely ignore this error by using the @ts-ignore directive
+  // @docs — https://stripe.com/docs/api/versioning
   // @ts-ignore
   const stripe = new Stripe(apiKey, { apiVersion })
+
+  // Store the initialized Stripe instance in the event context for future use
   event.context._stripe = stripe
 
   return event.context._stripe
