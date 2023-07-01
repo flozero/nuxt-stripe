4075e0144102585c4fed37db012aa57206fee12b
Loic Fuentes <fuentes.loic@gmail.com>
2023-07-01 11:22:14 -0400

    chore: clean repo links

--- src/runtime/composables/useClientStripe.ts
+++ src/runtime/composables/useClientStripe.ts
@@ -3,7 +3,7 @@ import { useNuxtApp } from "#imports"
 /**
  * useClientStripe function
  *
- * This function is a helper to easily access the Stripe instance provided by the Nuxt 3 plugin.
+ * This function is a helper to easily access the Stripe instance provided by the Nuxt plugin.
  * It can be used in components or pages to interact with the Stripe.js library.
  * 
  */
--- src/runtime/server/services/useServerStripe.ts
+++ src/runtime/server/services/useServerStripe.ts
@@ -4,10 +4,10 @@ import { H3Event } from 'h3'
 
 /**
  * useServerStripe is a utility function that initializes and returns a Stripe instance
- * for server-side usage in Nuxt 3. It ensures that only one instance of Stripe is created
+ * for server-side usage in Nuxt. It ensures that only one instance of Stripe is created
  * per event context, avoiding unnecessary re-initializations.
  *
- * @param {H3Event} event - The event object passed to the Nuxt 3 event handler
+ * @param {H3Event} event - The event object passed to the Nuxt event handler
  * @return {Promise<Stripe>} - A Promise that resolves to the Stripe server instance for the event context
  */
 export const useServerStripe = async(event: H3Event): Promise<Stripe> => {
